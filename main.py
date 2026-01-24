43adefc601275593b9610eb4ba71e625b88803e1
lucatoffanin20-cmd <lucatoffanin20@gmail.com>
2026-01-24 01:43:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
-import time
+
+# fallback manuale per evitare crash se Railway non passa la variabile
+DISCOGS_USER = os.getenv("DISCOGS_USER") or "tuo_username_discogs"
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+DISCOGS_USER_TOKEN = os.getenv("DISCOGS_USER_TOKEN")
+
 
 print("===== DEBUG ENV START =====")
 
